9135cd89eb762581b4b06266149ac13a9f475195
Andrew Tran <andrewdaotran@gmail.com>
2023-05-10 11:43:00 -0700

    Fixed resizable message box in ChatboxSmall and allow message to be string or null type

--- src/components/ChatboxSmall.tsx
+++ src/components/ChatboxSmall.tsx
@@ -1,18 +1,16 @@
 import { johnnyAge } from "andrewdaotran/utils/johnnyInfo";
-import { useState, ChangeEvent, useRef, useEffect } from "react";
+import { useState, ChangeEvent, useRef, FormEvent } from "react";
 import MessageBox from "./MessageBox";
 
 export const ChatboxSmall = () => {
-  const [message, setMessage] = useState<string>("");
+  const [message, setMessage] = useState<string | null>("");
 
   const textareaRef = useRef(null);
-  const textRowCount = textareaRef ? message.split("\n").length : 0;
-  const rows = textRowCount + 1;
-  // const rows = textRowCount < 6 ? textRowCount : 6;
 
-  const typeMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(e.target.value);
+  const typeMessage = (e: ChangeEvent<HTMLSpanElement>) => {
+    setMessage(e.currentTarget.textContent);
   };
+
   return (
     <main className="mx-16 min-h-screen w-full flex-col rounded-lg border border-black">
       {/* Top */}
@@ -20,19 +18,25 @@ export const ChatboxSmall = () => {
         <h1 className="font-semibold">Johnny, {johnnyAge()}</h1>
         <h2 className="text-sm text-gray-400">Orange County</h2>
       </div>
+      {/* Top End */}
+
       {/* Chat Area */}
-      <div className=" flex-2 mb-2  flex flex-col-reverse gap-2 overflow-auto scroll-smooth border-t border-gray-200 px-2">
+      <div className=" flex-2 mb-2 flex h-full flex-col-reverse gap-2 overflow-auto scroll-smooth border-t border-gray-200 px-2">
         <MessageBox />
       </div>
+      {/* Chat Area End */}
+
       {/* Message Box */}
-      <textarea
-        className="h-full w-full flex-auto resize-none rounded-xl border bg-gray-100 px-2 py-1 focus:outline-none"
-        ref={textareaRef}
-        value={message}
-        onChange={typeMessage}
-        rows={rows}
-        placeholder="Message"
-      />
+      <p className="mx-2">
+        <span
+          className="block h-fit w-[100%] flex-auto resize-none rounded-xl border bg-gray-100 p-2 empty:before:text-gray-400 empty:before:content-['Message'] focus:outline-none"
+          ref={textareaRef}
+          onInput={typeMessage}
+          placeholder="Message"
+          contentEditable
+        />
+      </p>
+      {/* Message Box End */}
     </main>
   );
 };
