10ad4ed052fce3ab6121e4b9f6af38e5544ed4e6
Andrew Tran <andrewdaotran@gmail.com>
2024-02-20 14:12:49 -0800

    Fix ChatboxContext submitMessage to actually add message to allMessages

--- src/context/ChatboxContext.tsx
+++ src/context/ChatboxContext.tsx
@@ -1,3 +1,4 @@
+import { all } from "axios";
 import {
   ChangeEvent,
   FormEvent,
@@ -21,17 +22,26 @@ const ChatboxContext = createContext<ChatboxContextType | null>(null);
 export const ChatboxProvider = ({ children }: ChildrenNodeType) => {
   const [userMessage, setUserMessage] = useState<string | null>("");
   // Code to send messages to python code
+  const [allMessages, setAllMessages] = useState([
+    {
+      message: "",
+      user: "",
+      timeStamp: "",
+    },
+  ]);
 
   const submitMessage = (
     e: FormEvent,
     textareaRef: RefObject<HTMLSpanElement>
   ) => {
     e.preventDefault();
-    if (textareaRef.current !== null) {
-      textareaRef.current!.textContent = "";
-      return;
-    }
-    setAllMessages([
+    // if (textareaRef.current !== null) {
+    //   textareaRef.current!.textContent = "";
+    //   return;
+    // }
+    if (userMessage?.replaceAll(" ", "").length === 0) return;
+
+    setAllMessages((allMessages) => [
       ...allMessages,
       {
         message: userMessage!,
@@ -39,6 +49,8 @@ export const ChatboxProvider = ({ children }: ChildrenNodeType) => {
         timeStamp: new Date().toLocaleTimeString(),
       },
     ]);
+    setUserMessage("");
+    textareaRef.current!.textContent = "";
   };
 
   // Code to receive messages from python code
@@ -47,15 +59,7 @@ export const ChatboxProvider = ({ children }: ChildrenNodeType) => {
     setUserMessage(e.currentTarget.textContent);
   };
 
-  console.log(userMessage);
-
-  const [allMessages, setAllMessages] = useState([
-    {
-      message: "",
-      user: "",
-      timeStamp: "",
-    },
-  ]);
+  console.log(userMessage, allMessages);
   return (
     <ChatboxContext.Provider
       value={{ submitMessage, allMessages, typeUserMessage }}
